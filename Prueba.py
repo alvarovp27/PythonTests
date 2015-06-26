c8d3fafc636db3c16a803181aa85fce4cc25812d
alvarovp27 <avalenciaparra@gmail.com>
2015-06-26 23:58:56 +0200

    Prueba1

---
+++ Prueba.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-         #ASÍ SE ADMITEN LOS CARACTERES ESPECIALES
+__author__ = 'Alvaro'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+a="¡Hola, mundo!"
+
+for i in range(0,10,1):
+    print(a+" Iteración: "+i.__str__())
+
